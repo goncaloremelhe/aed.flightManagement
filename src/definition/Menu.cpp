65aa9eaa7f21ed33e0e7d546f1e9632a136b1307
AmandaTartarotti <amandatartarotti@gmail.com>
2023-12-17 12:22:54 -0400

    Add numFlights per airport

--- src/definition/Menu.cpp
+++ src/definition/Menu.cpp
@@ -1,13 +1,15 @@
 
 #include "../header/Menu.h"
 #include <iostream>
+#include <set>
 
 
 void printStatisticsMenu(const FlightManagement& flightManagement){
     cout << "--------------------------------------------------\n";
     cout << "Choose one option:" << endl;
-    cout << "1 - Check global number of airports;" << endl;
-    cout << "2 - Check global number of available flights;" << endl;
+    cout << "1 - Check global number of airports" << endl;
+    cout << "2 - Check global number of available flights" << endl;
+    cout << "3 - Check number of flights of an airport" << endl;
     cout << "--------------------------------------------------\n";
     cout << "Option: ";
     int option = 0;
@@ -28,6 +30,30 @@ void printStatisticsMenu(const FlightManagement& flightManagement){
             }
 
             cout << "The global number of available flights is " << numFlights << '.' << endl; }
+            break;
+        case 3:{
+            cout << "What is the code of the airport? [XXX] " << endl;
+            cout << "Airport Code: ";
+            string airportCode;
+            cin >> airportCode;
+
+            Graph<string> graph = flightManagement.getGraph();
+            vector<Vertex<string>*> nodes = graph.getVertexSet();
+
+            for (auto node : nodes){
+                if(node->getInfo() == airportCode){
+                    cout << "The number of flights out of the Airport " + airportCode + " is " + to_string(node->getAdj().size()) << endl;
+
+                    set<string> uniqueAirlines;
+                    for (auto edge : node->getAdj()) {
+                        uniqueAirlines.insert(edge.getAirline());
+                    }
+
+                    int numberOfAirlines = uniqueAirlines.size();
+                    cout << " Those flights are from " << numberOfAirlines << " different airlines." << endl;
+                }
+            }
+        }
             break;
         default:
             cout << "Invalid option. Exiting." << endl;
